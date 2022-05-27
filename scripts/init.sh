2df3b3afae508004d43417da988d4cf15268fb84
Pietro Pasotti <starfire.daemon@gmail.com>
2022-05-27 12:19:47 +0200

    fixed script

--- scripts/init.sh
+++ scripts/init.sh
@@ -26,17 +26,22 @@ fill_in "\$CHARM_NAME" "$CHARM_NAME" "./metadata.yaml"
 fill_in "\$LIB_NAME" "$LIB_NAME" "./tox.ini"
 
 echo "registering charm $CHARM_NAME"
-charmcraft register "$CHARM_NAME"  # create the 'charm' placeholder
+# create the 'charm' placeholder
+charmcraft register "$CHARM_NAME"
 
 echo "registering lib $LIB_NAME"
-charmcraft create-lib "$LIB_NAME"  # register the lib to that charm
-touch  "./$LIB_NAME.py" # create the source file for the lib
+# register the lib to that charm
+charmcraft create-lib "$LIB_NAME"
+# create the source file for the lib
+touch  "./$LIB_NAME.py"
 
+# extract LIBID
 LIBID_RAW=$(cat "./lib/charms/$CHARM_NAME/$LIB_VERSION/$LIB_NAME.py" | grep LIBID)
-LIBID=${a#*LIBID = }  # extract LIBID
+LIBID=${LIBID_RAW#*LIBID = }
 
 fill_in "\$LIBID" "$LIBID" "lib_template.jinja"
 
-rm "./lib/charms/$CHARM_NAME/$LIB_VERSION/$LIB_NAME.py" # get rid of the lib file
+# get rid of the lib file
+rm "./lib/charms/$CHARM_NAME/$LIB_VERSION/$LIB_NAME.py"
 
 echo "lib ready at lib/charms/$CHARM_NAME/$LIB_VERSION/$LIB_NAME! Happy coding."

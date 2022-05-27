d9e945598245719beddf9f43f0aa4c7fff24bc23
Pietro Pasotti <starfire.daemon@gmail.com>
2022-05-27 09:37:42 +0200

    fixed script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,26 +1,39 @@
 #!/usr/bin/env bash
+export CHARM_NAME=$1
+export LIB_NAME=$2
 
-export LIB_NAME=$1
+[ -z "$LIB_NAME" ] && echo "need to provide LIB_NAME as first argument" && exit 1
+[ -z "$CHARM_NAME" ] && echo "need to provide CHARM_NAME as second argument" && exit 1
 
-charmcraft register "$LIB_NAME"  # create the 'charm' placeholder
-charmcraft create_lib "$LIB_NAME"  # register the lib to that charm
+
+
+function fill_in() {
+    echo "$3:: replacing $1 -> $2"
+    sed -i "s/$1/$2/g" "$3"
+}
+
+# populate templates
+fill_in "\$LIBID" "$LIBID" "lib_template.jinja"
+fill_in "\$LIB_NAME" "$LIB_NAME" "./scripts/publish.sh"
+fill_in "\$CHARM_NAME" "$CHARM_NAME" "./scripts/publish.sh"
+
+fill_in "\$LIB_NAME" "$LIB_NAME" "./scripts/inline-lib.py"
+fill_in "\$CHARM_NAME" "$CHARM_NAME" "./scripts/inline-lib.py"
+
+fill_in "\$LIB_NAME" "$LIB_NAME" "./metadata.yaml"
+fill_in "\$CHARM_NAME" "$CHARM_NAME" "./metadata.yaml"
+fill_in "\$LIB_NAME" "$LIB_NAME" "./tox.ini"
+
+echo "registering charm $CHARM_NAME"
+charmcraft register "$CHARM_NAME"  # create the 'charm' placeholder
+
+echo "registering lib $LIB_NAME"
+charmcraft create-lib "$LIB_NAME"  # register the lib to that charm
 touch  "./$LIB_NAME.py" # create the source file for the lib
 
 LIBID_RAW=$(cat "./lib/charm/$LIB_NAME/v0/$LIB_NAME.py" | grep LIBID)
 LIBID=${a#*LIBID = }  # extract LIBID
 
 rm "./lib/charm/$LIB_NAME/v0/$LIB_NAME.py" # get rid of the lib file
-
-function fill_in() {
-    sed -i "s/$2/$3/g" "$1"
-}
-
-# populate templates
-fill_in "\$LIBID" "$LIBID" "lib_template.jinja"
-fill_in "\$LIB_NAME" "$LIB_NAME" "./scripts/publish.sh"
-fill_in "\$LIB_NAME" "$LIB_NAME" "./scripts/inline-lib.sh"
-fill_in "\$LIB_NAME" "$LIB_NAME" "./metadata.yaml"
-fill_in "\$LIB_NAME" "$LIB_NAME" "./tox.ini"
-
 
 echo "lib ready at lib/charms/$LIB_NAME/v0/$LIB_NAME! Happy coding."
